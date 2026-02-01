b7a0a3b7a0fa8fe7324a12b697894581a1df349f
Mike <172455720+polarbots@users.noreply.github.com>
2026-02-01 17:23:43 -0500

    Completed Ex.07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    // Split string with ""
+    let strArr = word.split("");
 
+    // Call array.reverse()
+    strArr.reverse();
+
+    // Call array.join()
+    return reversedString = strArr.join("");
 };
 
 // Do not edit below this line
