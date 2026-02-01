85f7a4e7f5f2cb687ee648b6a9f60144317dd1a3
Mike <172455720+polarbots@users.noreply.github.com>
2026-02-01 17:18:41 -0500

    Completed Ex.06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    // If num is negative, return `ERROR`
+    if (num < 0) {
+        return `ERROR`;
+    }
 
+    // Create a for loop from i = 0 to num
+    // Add string to itself
+    let newString = "";
+    for (let i = 0; i < num; i++) {
+        newString += string;
+    }
+
+    // return string after loop
+    return newString;
 };
 
 // Do not edit below this line
