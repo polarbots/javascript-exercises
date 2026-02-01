0d82b03c0c91979a60ac4c82b9419ee8d9a04f76
Mike <172455720+polarbots@users.noreply.github.com>
2026-02-01 18:15:32 -0500

    Completed Ex.08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numToRemoveArr) {
+
+    // Filter for if num is not in numToRemoveArr
+    // Resulting array will be array with numbers "removed"
+    return arr.filter((num) => !numToRemoveArr.includes(num));
+
 };
 
 // Do not edit below this line
