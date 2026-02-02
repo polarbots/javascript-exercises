505f4caab1395464a92c4ee7ed7e39aa3d7e37f1
Mike <172455720+polarbots@users.noreply.github.com>
2026-02-02 18:49:52 -0500

    Completed Ex.10

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,4 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    // Conditions:
+    // Is leap year:
+    // Divisible by 4 and not by 100 -> year % 4 == 0 && !(year % 100 == 0)
+    if (year % 4 == 0 && !(year % 100 == 0)) return true;
+
+    // Divisible by 4, by 100, and by 400 -> year % 4 == 0 && year % 100 == 0 && year % 400 == 0
+    if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) return true;
+
+    // Else return false
+    return false;
+
+    // One liner way
+    // if (year % 4 == 0 && (!(year % 100 == 0)) || year % 400 == 0) return true;
 
 };
 
