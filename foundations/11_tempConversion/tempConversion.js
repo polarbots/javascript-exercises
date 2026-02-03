b8273fdec45be6819013890be28e7eb180e01eb1
Mike <172455720+polarbots@users.noreply.github.com>
2026-02-02 19:08:28 -0500

    Completed Ex.11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  //  temp-32 * (5/9)
+  // Round to 1 dp
+  let tempInC = (tempInF - 32) * (5/9);
+  return Math.round(tempInC*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  // temp * (9/5) + 32 
+  // Round to 1 dp
+  let tempInF = tempInC * (9/5) + 32;
+  return Math.round(tempInF*10)/10;
 };
 
 // Do not edit below this line
