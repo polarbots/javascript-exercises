48ce7910d3dd74d29b4da1139e139eabdcb68bd6
Mike <172455720+polarbots@users.noreply.github.com>
2026-03-22 17:58:41 -0400

    Completed exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, num) => total + num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => total * num, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = num;
+	if (num === 0) {
+    return 1
+  } else {
+    for (let i = num; i > 1; i--) {
+      result *= i-1;
+    }
+    return result
+  }
 };
 
 // Do not edit below this line
