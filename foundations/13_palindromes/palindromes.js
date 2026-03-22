518b4a6813f2c5d01db83ec2d0d7010db28ba7f3
Mike <172455720+polarbots@users.noreply.github.com>
2026-03-22 19:14:28 -0400

    Completed exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Compare first and last letter and then move first and last pointer forward and back respectively
+    // First remove all spaces and punctuation
+    let cleanStr = str.replace(/[^a-zA-Z0-9]/g, "");
+
+    // Make it all lower case
+    let cleanStrLower = cleanStr.toLowerCase()
+
+    for (let left = 0, right = cleanStrLower.length-1; left < right; left++, right--) {
+        if (cleanStrLower[left] !== cleanStrLower[right]) {
+            return false
+        }
+    }
+    return true
+
+    // Or I could just reverse the string and compare
 
 };
 
