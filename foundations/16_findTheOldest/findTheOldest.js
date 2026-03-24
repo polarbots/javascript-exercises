e12c034e1d48266e5ee5556911c35046a3e605e3
Mike <172455720+polarbots@users.noreply.github.com>
2026-03-23 22:16:01 -0400

    Completed exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const getAge = function(birthYear, deathYear) {
+    if (!deathYear) {
+        deathYear = new Date().getFullYear();
+    }
+    return deathYear - birthYear
+}
 
+const findTheOldest = function(arr) {
+
+    return arr.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        // If age of current person is larger than existing older age, return the current person, if not, then return the existing oldest person
+        // The value return is then assigned to "oldest", and is ready for the next "loop" of the array until we find the oldest person
+        return oldestAge < currentAge ? currentPerson : oldest
+
+    })
 };
 
 // Do not edit below this line
