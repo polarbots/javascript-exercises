458f5024e721e97a832067241ae1975efea9465d
Mike <172455720+polarbots@users.noreply.github.com>
2026-03-23 21:24:21 -0400

    Completed exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,35 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    // Convert input to int
+    const numInt = Math.round(input)
 
+    if (numInt < 0) {
+        return "OOPS"
+    }
+
+    // // Using Binet's formula
+    // const goldenRatio = (1+5**0.5)/2;
+    // const numerator = goldenRatio**numInt - (1-goldenRatio)**numInt;
+    // return Math.round(numerator/(5**0.5));
+
+    const zeroNum = 0;
+    const firstNum = 1;
+    let result = 1;
+    let prevPrevResult = zeroNum;
+    let prevResult = firstNum;
+
+    if (numInt === 0) {
+        return zeroNum
+    } else if (numInt === 1) {
+        return firstNum
+    } else {
+        for (let i = 2; i <= numInt; i++) {
+            result = prevResult + prevPrevResult;
+            prevPrevResult = prevResult;
+            prevResult = result;
+
+        }
+        return prevResult
+    }
 };
 
 // Do not edit below this line
